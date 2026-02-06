0450cd1efad4f4e682dd56a13973bfa1687560d5
alwinsjostrom <alwinsjostrom@yahoo.com>
2026-02-06 14:39:37 +0100

    Setup tailwind in web app, added ui to workspace, import first ui component

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import { Hello } from '@repo/ui';
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <Hello />
 </template>
 
 <style scoped>
